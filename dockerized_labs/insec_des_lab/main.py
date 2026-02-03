5c218ce9fb1b772ef4097df571cb8119c26fb6d7
monserratvela-hash <monserratvela@gmail.com>
2026-02-02 22:09:43 -0300

    Update main.py

--- dockerized_labs/insec_des_lab/main.py
+++ dockerized_labs/insec_des_lab/main.py
@@ -33,8 +33,7 @@ def deserialize_data():
         serialized_data = request.form.get('serialized_data', '')
         decoded_data = base64.b64decode(serialized_data)
         # Intentionally vulnerable deserialization, matching PyGoat
-        user = json.loads(decoded_data)
-        #remediacion CWE 502 user = pickle.loads(decoded_data)
+        user = pickle.loads(decoded_data)
         
         if isinstance(user, User):
             if user.is_admin:
